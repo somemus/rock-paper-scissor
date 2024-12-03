d04c71a892dd073b692d0d744b1baff202f84d6e
somemus <184270975+somemus@users.noreply.github.com>
2024-12-03 19:39:17 +0300

    Edit function
    Use Mathrandom and conditional switch for choice instead of OR operator

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-console.log("Hello World")
-function getComputerChoice (hello){
-    return "scissor" || "paper" ||"rock" ;
+
+function getComputerChoice (){
+    let choice = Math.floor(Math.random() * 3);
+    switch(choice) {
+        case '0':
+            return('scissor')
+        case '1':
+            return('rock')
+        case '2':
+            return('paper')
+    }
+   
+
+   
 }
 
 b = getComputerChoice()
